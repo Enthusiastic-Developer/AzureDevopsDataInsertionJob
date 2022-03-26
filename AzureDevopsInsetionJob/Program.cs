dd1bc11c273c5ad11f2a2c24c860a1c93539c23a
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2022-03-26 09:28:06 +0530

    Added job templent

---
+++ AzureDevopsInsetionJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureDevopsInsetionJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
