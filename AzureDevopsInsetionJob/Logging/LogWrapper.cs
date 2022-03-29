6da8db2ec779ac66f6b80bb7e4140ba9883cd184
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2022-03-29 16:01:54 +0530

    Logging is added

---
+++ AzureDevopsInsetionJob/Logging/LogWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+
+namespace AzureDevopsInsetionJob.Logging
+{
+    public class LogWrapper
+    {
+        private readonly ILogger<LogWrapper> _logger;
+        public LogWrapper(ILogger<LogWrapper> logger)
+        {
+            _logger = logger;
+        }
+        public void LevelLogger (string level, string msg)
+        {
+            if (level == "Trace")
+                Trace(msg);
+            else if (level == "Info")
+                Info(msg);
+            else if (level == "Error")
+                Error(msg);
+            else if (level == "Debug")
+                Debug(msg);
+            else if (level == "Warn")
+                Warn(msg);
+        }
+
+        private void Warn(string msg)
+        {
+            _logger.LogWarning(11,"{msg}",msg);
+        }
+
+        private void Debug(string msg)
+        {
+            _logger.LogDebug(12, "{msg}", msg);
+        }
+
+        private void Error(string msg)
+        {
+            _logger.LogError(13, "{msg}", msg);
+        }
+
+        private void Info(string msg)
+        {
+            _logger.LogInformation(14, "{msg}", msg);
+        }
+
+        private void Trace(string msg)
+        {
+            try
+            {
+                _logger.LogTrace(15, "{msg}", msg);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }        
+        }
+    }
+}
